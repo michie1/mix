89df94eb9d7261608c4c21df78388b70e0003306
Michiel <michiel@blue-donut.com>
2017-12-12 20:20:06 +0100

    Library filter

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,15 @@
 module Main exposing (main)
 
-import Html exposing (Html, program, text, div, ul, li, h2)
-import Html.Events exposing (onClick)
-import Html.Attributes exposing (style)
+import Html exposing (Html, program, text, div, ul, li, h2, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (style, value)
+import List.Extra
 
 
 type alias Model =
     { library : List Track
     , playlist : List Track
+    , libraryFilter : String
     }
 
 
@@ -28,6 +30,7 @@ init =
     in
         ( { library = [ track ]
           , playlist = []
+          , libraryFilter = ""
           }
         , Cmd.none
         )
@@ -36,19 +39,42 @@ init =
 type Msg
     = NoOp
     | SelectTrack Int Int
-    | DeselectTrack Int Int
+    | DeselectTrack Int
+    | LibraryFilter String
+
+iContains : String -> String -> Bool
+iContains a b =
+    String.contains (String.toLower a) (String.toLower b)
+
+filter : String -> List Track -> List Track
+filter query tracks =
+    List.filter
+        (\track -> String.trim query == "" ||
+                   iContains query (toString track.cd) ||
+                   iContains query (toString track.number) ||
+                   iContains query track.artist ||
+                   iContains query track.title ||
+                   iContains query track.mix
+        )
+        tracks
 
 
 view : Model -> Html Msg
 view state =
     div []
         [ h2 [] [ text "Library" ]
+        , libraryFilter state.libraryFilter
         , ul [] <|
-            List.map libraryLi state.library
-        , h2 [] [ text "Library" ]
+            List.map libraryLi (filter state.libraryFilter state.library)
+        , h2 [] [ text "Playlist" ]
         , ul [] <|
-            List.map playlistLi state.playlist
+            List.map playlistLi (List.Extra.zip (List.range 0 (List.length state.playlist)) state.playlist)
         ]
+
+
+libraryFilter : String -> Html Msg
+libraryFilter query =
+    input [ value query, onInput LibraryFilter ] []
 
 
 libraryLi : Track -> Html Msg
@@ -56,9 +82,16 @@ libraryLi track =
     li [ onClick <| SelectTrack track.cd track.number ] [ text <| track.artist ++ " - " ++ track.title ++ " (" ++ track.mix ++ ")" ]
 
 
-playlistLi : Track -> Html Msg
-playlistLi track =
-    li [ onClick <| DeselectTrack track.cd track.number, style [ ( "list-style-type", "decimal" ) ] ] [ text <| track.artist ++ " - " ++ track.title ++ " (" ++ track.mix ++ ")" ]
+playlistLi : ( Int, Track ) -> Html Msg
+playlistLi index_track =
+    let
+        index =
+            Tuple.first index_track
+
+        track =
+            Tuple.second index_track
+    in
+        li [ onClick <| DeselectTrack index, style [ ( "list-style-type", "decimal" ) ] ] [ text <| track.artist ++ " - " ++ track.title ++ " (" ++ track.mix ++ ")" ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -69,8 +102,7 @@ update msg state =
                 playlist =
                     case
                         state.library
-                            |> List.filter (\t -> t.cd == cd && t.number == t.number)
-                            |> List.head
+                            |> List.Extra.find (\t -> t.cd == cd && t.number == t.number)
                     of
                         Just track ->
                             List.append state.playlist [ track ]
@@ -80,12 +112,17 @@ update msg state =
             in
                 ( { state | playlist = playlist }, Cmd.none )
 
-        DeselectTrack cd number ->
+        DeselectTrack index ->
             let
                 playlist =
-                    List.filter (\t -> t.cd /= cd || t.number /= t.number) state.library
+                    List.Extra.removeAt index state.playlist
+
+                -- List.filter (\t -> t.cd /= cd || t.number /= t.number) state.library
             in
                 ( { state | playlist = playlist }, Cmd.none )
+
+        LibraryFilter query ->
+            ( { state | libraryFilter = query }, Cmd.none )
 
         NoOp ->
             ( state, Cmd.none )
